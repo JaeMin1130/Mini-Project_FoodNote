3f28b4f37577b24f844a0b7b9fea915a5fbd2208
JaeMin1130 <ija06598@gmail.com>
2023-07-27 10:40:28 +0900

    07/27

--- back/src/main/java/edu/pnu/service/UserService.java
+++ back/src/main/java/edu/pnu/service/UserService.java
@@ -11,25 +11,25 @@ import org.springframework.stereotype.Service;
 import edu.pnu.domain.User;
 import edu.pnu.persistance.UserRepository;
 
-@Service
-public class UserService implements UserDetailsService {
+//@Service
+//public class UserService implements UserDetailsService {
+//
+//	@Autowired
+//	UserRepository userRepo;
+//	
+//	@Override
+//	public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
+//		
+//		Optional <User> option = userRepo.findById(userId);
+//		if (!option.isPresent()) {
+//			throw new UsernameNotFoundException("사용자가 없습니다.");
+//		}
+//		
+//		User user = option.get();
+//		
+//		System.out.println(user);
+//		
+//		return new User(user.getUserId(), user.getPassword(), user.getAuthorities());
+//	}
 
-	@Autowired
-	UserRepository userRepo;
-	
-	@Override
-	public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
-		
-		Optional <User> option = userRepo.findById(userId);
-		if (!option.isPresent()) {
-			throw new UsernameNotFoundException("사용자가 없습니다.");
-		}
-		
-		User user = option.get();
-		
-		System.out.println(user);
-		
-		return new User(user.getUsername(), user.getPassword(), user.getAuthorities());
-	}
-
-}
+//}
