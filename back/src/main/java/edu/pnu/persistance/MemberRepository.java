9926e6cb90fe9c4cd1ff9d44bd6084e8d3b330b0
JaeMin1130 <ija06598@gmail.com>
2023-08-07 09:37:14 +0900

    08/07

--- back/src/main/java/edu/pnu/persistance/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package edu.pnu.persistance;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import edu.pnu.domain.Member;
-
-public interface MemberRepository extends JpaRepository<Member, String> {
-	
-}
