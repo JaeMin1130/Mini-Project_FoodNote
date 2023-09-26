4c2f80747572a3da45beb8b78dbc7c30e54ecc48
JaeMin1130 <ija06598@gmail.com>
2023-09-26 20:02:15 +0900

    EB test : 도메인 접속

---
+++ src/main/java/edu/pnu/controller/TestController.java
@@ -0,0 +1,24 @@
+package edu.pnu.controller;
+
+import org.springframework.boot.actuate.health.HealthEndpoint;
+import org.springframework.boot.actuate.health.Status;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    private final HealthEndpoint healthEndpoint;
+
+    public TestController(HealthEndpoint healthEndpoint) {
+        this.healthEndpoint = healthEndpoint;
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<Status> healthStatus() {
+        Status status = healthEndpoint.health().getStatus();
+        HttpStatus httpStatus = Status.UP.equals(status) ? HttpStatus.OK : HttpStatus.INTERNAL_SERVER_ERROR;
+        return ResponseEntity.status(httpStatus).body(status);
+    }
+}
