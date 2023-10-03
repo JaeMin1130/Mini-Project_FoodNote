0dbac215410504b296cc8afbbc9ea8914b10ff63
JaeMin1130 <ija06598@gmail.com>
2023-10-03 17:34:20 +0900

    Github Action with Doker Test6

--- src/main/java/edu/pnu/config/WebConfig.java
+++ src/main/java/edu/pnu/config/WebConfig.java
@@ -14,7 +14,8 @@ public class WebConfig implements WebMvcConfigurer {
 		// 모든 경로에 대하여
 		registry.addMapping("/**")
 				// Origin이 http://localhost:3000에 대해.
-				.allowedOrigins("https://jaemin1130.github.io", "http://localhost:3000", "http://10.125.121.173:3000")
+				.allowedOrigins("https://jaemin1130.github.io", "http://localhost:3000",
+						"http://mealnote-env.eba-yyabenet.ap-northeast-2.elasticbeanstalk.com:5000")
 				// GET, POST, PUT, DELETE 메서드를 허용한다.
 				.allowedMethods("*")
 				// 모든 헤더와 인증에 관한 정보도 허용한다.
