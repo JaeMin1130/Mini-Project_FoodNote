abfcbfe3f68048b0ab7c355c49a2af71f9d0eb5b
JaeMin1130 <ija06598@gmail.com>
2023-10-04 05:05:35 +0900

    10/04 로컬 호스트에서 배포한 호스트로 변경

--- src/api/api-config.js
+++ src/api/api-config.js
@@ -1,8 +1,5 @@
 let backendHost;
 
-// const hostname = window && window.location && window.location.hostname;
-
-// backendHost = "https://10.125.121.173:8080";
-backendHost = "http://localhost:8080";
+backendHost = "http://15.164.78.46";
 
 export const API_BASE_URL = `${backendHost}`;
