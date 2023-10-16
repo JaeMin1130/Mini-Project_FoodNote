b1f6c096fdd1be471ccd19b9b55ce8be490cf06e
JaeMin1130 <ija06598@gmail.com>
2023-10-16 09:31:32 +0900

    배포된 서버 시간 설정3

--- src/main/java/edu/pnu/BigDataApplication.java
+++ src/main/java/edu/pnu/BigDataApplication.java
@@ -1,12 +1,22 @@
 package edu.pnu;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class BigDataApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BigDataApplication.class, args);
+	}
+
+	// Bean 생명주기를 이용한 timezone 설정
+	@PostConstruct
+	public void setTimeZone() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 }
